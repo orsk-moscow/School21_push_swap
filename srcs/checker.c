e28700926c42c6f5c45a76e8e3fd56a36ba55a27
orsk-moscow <klekisha@student.21-school.com>
2020-02-17 19:16:20 +0300

    stack adding correct

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,7 +6,7 @@
 /*   By: u18188899 <u18188899@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 18:20:59 by u18188899         #+#    #+#             */
-/*   Updated: 2020/02/17 08:49:16 by u18188899        ###   ########.fr       */
+/*   Updated: 2020/02/17 19:15:24 by u18188899        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 
 int main(int ac, char **av)
 {
-	int res;
+	int			res;
+	t_list		*stck_a;
+	t_list		*stck_b;
 
-	while (ac-- > 0)
+	// printf("size of int = %zu\n",sizeof(int)); // 4 bytes
+	res = ft_atoi(av[--ac]);
+	stck_a = ft_lstnew(&res, sizeof(int));
+	while (--ac > 0)
 	{
-		res = ft_atoi(*(av++));
-		printf("%i\n",res);
+		// printf("%i\n",ac);
+		// printf("%s\n",av[ac]);
+		res = ft_atoi(av[ac]);
+		ft_lstadd(&stck_a, ft_lstnew(&res, sizeof(int)));
+		// printf("%i\n",res);
+	}
+	while (stck_a)
+	{
+		printf("content of chain element: %i\n",*((int*)(stck_a->content)));
+		stck_a = stck_a->next;
 	}
 	return (0);
 }
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-cd libft
-make
-make clean
-cd ..
-gcc /Users/u18188899/School21_push_swap/srcs/checker.c /Users/u18188899/School21_push_swap/libft/libft.a
+# cd libft
+# make
+# make clean
+# cd ..
+gcc -g /Users/u18188899/School21_push_swap/srcs/checker.c /Users/u18188899/School21_push_swap/libft/libft.a
