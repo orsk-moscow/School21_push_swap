58061e8de7d13c2fcf14ed7c360b05613d5286f8
orsk-moscow <klekisha@student.21-school.com>
2020-03-13 23:26:22 +0300

    Delete 1 useless function

--- srcs/instructions6.c
+++ srcs/instructions6.c
@@ -41,39 +41,6 @@ t_stcks					*ft_do_rb_1(t_stcks *stcks)
 	return (stcks);
 }
 
-// /* ************************************************************************** */
-// t_stcks					*ft_do_rr_1(t_stcks *stcks)
-// {
-// 	size_t					len;
-// 	t_lst_p_s				*tmp1;
-// 	t_lst_p_s				*tmp2;
-
-// 	len = ft_lstlen_1(stcks->b->lst);
-// 	if (len > 1)
-// 	{
-// 		tmp1 = stcks->b->lst;
-// 		stcks->b->lst = stcks->b->lst->nxt;
-// 		tmp2 = stcks->b->lst;
-// 		tmp1->nxt = NULL;
-// 		while (tmp2->nxt)
-// 			tmp2 = tmp2->nxt;
-// 		tmp2->nxt = tmp1;
-// 	}
-// 	len = ft_lstlen_1(stcks->a->lst);
-// 	if (len < 2)
-// 	{
-// 		tmp1 = stcks->a->lst;
-// 		stcks->a->lst = stcks->a->lst->nxt;
-// 		tmp2 = stcks->a->lst;
-// 		tmp1->nxt = NULL;
-// 		while (tmp2->nxt)
-// 			tmp2 = tmp2->nxt;
-// 		tmp2->nxt = tmp1;
-// 	}
-// 	ft_putstr_fd("rr\n", (int)STDOUT_FILENO);
-// 	return (stcks);
-// }
-
 /* ************************************************************************** */
 t_stcks					*ft_do_rra_1_slnt(t_stcks *stcks)
 {
