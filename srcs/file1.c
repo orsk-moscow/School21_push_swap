4470715dac41572ba39eb163b7f1830ec4cf0b88
orsk-moscow <klekisha@student.21-school.com>
2020-03-13 23:27:16 +0300

    Deleted 1 more useless function

--- srcs/file1.c
+++ srcs/file1.c
@@ -33,39 +33,6 @@ t_stcks					*ft_do_rra_1(t_stcks *stcks)
 }
 
 /* ************************************************************************** */
-// t_stcks					*ft_do_rrr_1(t_stcks *stcks)
-// {
-// 	size_t					len;
-// 	t_lst_p_s				*tmp1;
-// 	t_lst_p_s				*tmp2;
-
-// 	len = ft_lstlen_1(stcks->b->lst);
-// 	if (len < 2)
-// 		return (stcks);
-// 	tmp2 = stcks->b->lst;
-// 	while (tmp2->nxt->nxt)
-// 		tmp2 = tmp2->nxt;
-// 	tmp1 = tmp2->nxt;
-// 	tmp2->nxt = NULL;
-// 	tmp1->nxt = stcks->b->lst;
-// 	stcks->b->lst = tmp1;
-
-// 	len = ft_lstlen_1(stcks->a->lst);
-// 	if (len < 2)
-// 		return (stcks);
-// 	tmp2 = stcks->a->lst;
-// 	while (tmp2->nxt->nxt)
-// 		tmp2 = tmp2->nxt;
-// 	tmp1 = tmp2->nxt;
-// 	tmp2->nxt = NULL;
-// 	tmp1->nxt = stcks->a->lst;
-// 	stcks->a->lst = tmp1;
-
-// 	ft_putstr_fd("rrr\n", (int)STDOUT_FILENO);
-// 	return (stcks);
-// }
-
-/* ************************************************************************** */
 t_stcks					*ft_do_rrb_1(t_stcks *stcks)
 {
 	size_t					len;
